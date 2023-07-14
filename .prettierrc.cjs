cb69eaf097258f2899f361b68daaa2a4c6346ac7
letsjo <hyunoh.jo@gmail.com>
2023-07-14 21:33:57 +0900

    [#7] Chore: init prettier configuration

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  bracketSpacing: true, // 객체 리터럴에서 key와 value 사이에 공백을 추가합니다.
+  singleQuote: true, // 문자열을 작은따옴표로 감쌉니다.
+  tabWidth: 2, // 탭의 너비를 2로 설정합니다.
+  trailingComma: 'all', // 객체나 배열의 마지막 요소 뒤에 쉼표를 붙일지 여부를 설정합니다.
+  printWidth: 100, // 한 줄의 최대 길이를 설정합니다.
+  endOfLine: 'auto', // 개행 문자를 어떤 것으로 할지를 설정하는 옵션
+  useTabs: false, // 탭 대신 스페이스를 사용합니다.
+  semi: true, // 문장 끝에 세미콜론을 붙입니다.
+  jsxSingleQuote: true, // JSX에서도 문자열을 작은따옴표로 감쌉니다.
+  arrowParens: 'always', // 화살표 함수의 인자가 하나일 때에도 괄호를 사용합니다.
+  formatOnSave: true, // 저장할 때마다 코드를 자동으로 정리합니다.
+};
