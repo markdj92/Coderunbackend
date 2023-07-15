ef67529cdc287f2469f2293837585e946930723f
letsjo <hyunoh.jo@gmail.com>
2023-07-16 01:46:54 +0900

    [#8] Feat: Init Login page 1차

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
 
+import { PATH_ROUTE, USER_TOKEN_KEY } from '@/constants';
+
+import { postLogin } from '@/apis/authApi';
+import { useAuthForm } from '@/hooks/useAuthForm';
+import { useMovePage } from '@/hooks/useMovePage';
+
 const Login = () => {
-  return <div>Login</div>;
+  const { emailRef, passwordRef, userAccount, handleAccountChange, isValidAccount } = useAuthForm();
+
+  const [goTodo] = useMovePage([PATH_ROUTE.lobby]);
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await postLogin(userAccount);
+      const { access_token: accessToken } = response.data;
+      if (accessToken) {
+        localStorage.setItem(USER_TOKEN_KEY, accessToken);
+        goTodo();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <h1>로그인</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          name='email'
+          data-testid='email-input'
+          ref={emailRef}
+          value={userAccount.email}
+          onChange={handleAccountChange}
+        />
+        <input
+          name='password'
+          data-testid='password-input'
+          ref={passwordRef}
+          value={userAccount.password}
+          onChange={handleAccountChange}
+          type='password'
+          autoComplete='off'
+        />
+        <button type='submit' data-testid='signin-button' disabled={!isValidAccount()}>
+          로그인하기
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
