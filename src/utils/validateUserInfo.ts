ac219cc360346fdd7b137f540c10740eb0b9a7af
letsjo <hyunoh.jo@gmail.com>
2023-07-16 01:41:24 +0900

    [#8] Feat: Add 유저 정보 유효성 관리 유틸

---
+++ src/utils/validateUserInfo.ts
@@ -0,0 +1,25 @@
+export class validateUserInfo {
+  static checkAccount(email: string, password: string): boolean {
+    return this.#checkEmail(email) && this.#checkPassword(password);
+  }
+
+  static #checkEmail(email: string): boolean {
+    return this.#hasAtSign(email);
+  }
+
+  static #checkPassword(password: string): boolean {
+    return this.#isMinimumLength(password);
+  }
+
+  static #isExistString(string: string): boolean {
+    return string.trim().length > 0;
+  }
+
+  static #hasAtSign(value: string): boolean {
+    return value.includes('@');
+  }
+
+  static #isMinimumLength(value: string): boolean {
+    return value.length >= 8;
+  }
+}
