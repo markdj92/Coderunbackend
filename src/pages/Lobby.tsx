6106c39b08de2ff2ec35fc07d05a93eaeb66ce25
letsjo <hyunoh.jo@gmail.com>
2023-07-16 03:14:40 +0900

    [#8] Feat: Add logout in Lobby

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,4 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
+import { PATH_ROUTE } from '@/constants';
+
+import { postLogout } from '@/apis/authApi';
+
 const Lobby = () => {
-  return <>Lobby</>;
+  const navigator = useNavigate();
+  const handleLogout = async () => {
+    if (confirm('정말 떠나실건가요?'))
+      try {
+        await postLogout();
+        navigator(PATH_ROUTE.login);
+        alert('로그아웃 되었습니다.');
+      } catch (error) {
+        console.error(error);
+      }
+  };
+
+  return (
+    <>
+      <button onClick={handleLogout}>로그아웃</button>
+    </>
+  );
 };
 export default Lobby;
