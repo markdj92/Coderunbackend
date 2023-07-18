ed7294cf27b7da8a9e69781ac0f3869f0d56ca08
letsjo <hyunoh.jo@gmail.com>
2023-07-19 03:43:35 +0900

    [#8] Feat: init set global style

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { Suspense } from 'react';
 
 import PageRouter from '@/routes';
 
+import GlobalStyle from './styles/GlobalStyle';
+
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
+      <GlobalStyle />
       <PageRouter />
     </Suspense>
   );
