e85b968e41ea672cf61fefe9cc6d5668862a39df
oneju <wonju.noh.24@gmail.com>
2023-07-20 05:31:03 +0900

    [#11] Feat: add userInfo type for badge

--- src/types/room.ts
+++ src/types/room.ts
@@ -2,3 +2,14 @@ export interface IChat {
   username: string;
   message: string;
 }
+
+export type userInfos = { [key: number]: userInfo };
+
+export type userInfo = {
+  nickname: string;
+  level: string;
+  imageSource: string;
+  isLock: string;
+  isUser: string;
+  isHost: string;
+};
