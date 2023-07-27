75c92d2030f09715d1983a05f23d6b027d16a142
letsjo <hyunoh.jo@gmail.com>
2023-07-28 00:55:42 +0900

    [#77] Feat: Add disabled option

--- src/components/public/CustomButton.tsx
+++ src/components/public/CustomButton.tsx
@@ -2,26 +2,57 @@ import styled from 'styled-components';
 
 type CustomButtonProps = {
   title: string;
-  isBorder?: boolean;
+  isDisabled?: boolean;
   onClick?: () => void;
 };
 
-const CustomButton = ({ title, isBorder = true, onClick }: CustomButtonProps) => {
+const CustomButton = ({ title, isDisabled = false, onClick }: CustomButtonProps) => {
+  if (isDisabled)
+    return (
+      <DisableContainer disabled={true}>
+        <TitleBox>{title}</TitleBox>
+      </DisableContainer>
+    );
   return (
-    <Container onClick={onClick} isborder={isBorder ? 'true' : 'false'}>
+    <Container onClick={onClick}>
       <TitleBox>{title}</TitleBox>
     </Container>
   );
 };
 
-const Container = styled.button<{ isborder: string }>`
+const DisableContainer = styled.button`
+  display: inline-flex;
+  padding: 24px 44px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 12px;
+  border: 1px solid rgba(70, 64, 198, 0.2);
+  background: rgba(70, 64, 198, 0.2);
+  cursor: default;
+`;
+
+const Container = styled.button`
   display: flex;
   padding: 24px 44px;
   justify-content: center;
   align-items: center;
   border-radius: 12px;
-  border: ${(props) => (props.isborder === 'true' ? '1px solid #4640c6' : 'none')};
+  border: 1px solid #4640c6;
   background: rgba(70, 64, 198, 0.2);
+
+  &:hover {
+    background: linear-gradient(
+        90deg,
+        rgba(70, 64, 198, 0) 0%,
+        rgba(70, 64, 198, 0.4) 31.07%,
+        rgba(70, 64, 198, 0.4) 72.03%,
+        rgba(70, 64, 198, 0.13) 100%
+      ),
+      rgba(112, 0, 255, 0.2);
+    box-shadow: 0px 0px 12px 0px rgba(78, 0, 244, 0.4);
+    backdrop-filter: blur(12px);
+  }
 `;
 
 const TitleBox = styled.div`
