88bb08d9534663169961003de631176c1131b354
markdj92 <markdj9205@gmail.com>
2023-07-31 22:21:59 +0900

    이미 닉네임이 설정되어 있다면 더 이상 닉네임 설정불가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -113,7 +113,12 @@ export class AuthService {
       }
       
       const user = await this.authModel.findOne({ email: email });
-      user.nickname = nickname;
+      
+      if (user.nickname !== null) {
+        throw new BadRequestException('이미 닉네임이 설정되어 있습니다.');
+      }
+
+       user.nickname = nickname;
       await user.save();
       return { message: 'success'};
     }
