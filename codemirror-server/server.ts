0a11e9d5f265d8a68cdee0c5beb8face49c8b6ef
Minji Lee <minji0479@gmail.com>
2023-08-03 04:53:32 +0900

    feat : yjs peertopeer

--- codemirror-server/server.ts
+++ codemirror-server/server.ts
@@ -1,122 +1,62 @@
-import { Server, Socket } from 'socket.io';
-import {ChangeSet, Text} from "@codemirror/state"
-import {Update} from "@codemirror/collab"
-
-const express = require('express');
-const cors = require('cors');
+/**
+ * @type {any}
+ */
+const WebSocket1 = require('ws');
 const http = require('http');
+const wss = new WebSocket1.Server({ noServer: true });
+const setupWSConnection = require('y-websocket/bin/utils').setupWSConnection;
 
-const app = express();
-app.use(cors());
+const rooms = new Map<string, any>();
 
-const server = http.createServer(app);
-
-app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-// server.listen(3000, '127.0.0.1', () => {
-//   console.log('Server running at http://127.0.0.1:3000/');
-// });
-
-interface document {
-	updates: Update[],
-	doc: Text,
-	pending: ((value: any) => void)[],
+function getRoom(nickname: string) {
+  let room = rooms.get(nickname);
+  if (!room) {
+    const Y = require('yjs');
+    const ydoc = new Y.Doc();
+    room = {
+      ydoc,
+      clients: new Set(),
+      // 여기에 추가로 방 관리에 필요한 데이터를 추가할 수 있음
+    };
+    rooms.set(nickname, room);
+  }
+  return room;
 }
 
-let documents = new Map<string, document>();
-
-documents.set('', {
-	updates: [],
-	pending: [],
-	doc: Text.of(['\n\n\nStarting doc!\n\n\n'])
-})
-
-let io = new Server(server, {
-	path: "/game",
-	cors: {
-		origin: "*",
-		methods: ["GET", "POST"]
-	}
+const server = http.createServer((request: any, response: any) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('okay');
 });
 
-function getDocument(title: string): document {
-	if (documents.has(title)) return documents.get(title)!;
-	const documentContent: document = {
-		updates: [],
-		pending: [],
-		doc: Text.of([`Hello World! It's Codelearn!`])
-	};
-	documents.set(title, documentContent);
-	return documentContent;
-}
+// wss.on('connection', setupWSConnection);
+wss.on('connection', (ws: any, req: any) => {
+	const docName = req.url.slice(1); // URL에서 문서 이름 추출
+	const room = getRoom(docName);
+	
+	room.clients.add(ws);
+	
+	ws.on('close', () => {
+		room.clients.delete(ws);
+  });
+  
+  // y-websocket의 setupWSConnection을 사용하여 yjs와 연결 설정
+  setupWSConnection(ws, {
+    ydoc: room.ydoc
+  });
+});
 
-// listening for connections from clients
-io.on('connection', (socket: Socket) => {
-    
-    socket.on('codingtest-join', (nickname: string) => {
-	try {
-		socket.join(nickname);
-	} catch {
-		return false;
-	}
-	console.log(`Socket ${socket.id} joined room ${nickname}`);
-		return true;
-    });
+server.on('upgrade', (request: any, socket: any, head: any) => {
+  // You may check auth of request here..
+  // See https://github.com/websockets/ws#client-authentication
+  /**
+   * @param {any} ws
+   */
+  const handleAuth = (ws: any) => {
+    wss.emit('connection', ws, request);
+  };
+  wss.handleUpgrade(request, socket, head, handleAuth);
+});
 
-	socket.on('pullUpdates', ({version, nickname}) => {
-		try {
-			const { updates, pending, doc } = getDocument(nickname);
-			if (version < updates.length) {
-				socket.emit("pullUpdateResponse", JSON.stringify(updates.slice(version)))
-			} else {
-				pending.push((updates) => { socket.emit('pullUpdateResponse', JSON.stringify(updates.slice(version))) });
-				documents.set(nickname, {updates, pending, doc})
-			}
-		} catch (error) {
-			console.error('pullUpdates', error);
-		}
-	})
-
-	socket.on('pushUpdates', ({ nickname, version, docUpdates }) => {
-		try {
-			let { updates, pending, doc } = getDocument(nickname);
-			docUpdates = JSON.parse(docUpdates);
-			if (version != updates.length) {
-				socket.emit('pushUpdateResponse', false);
-			} else {
-				for (let update of docUpdates) {
-					// Convert the JSON representation to an actual ChangeSet
-					// instance
-					let changes = ChangeSet.fromJSON(update.changes)
-					updates.push({ changes, clientID: update.clientID, effects: update.effects })
-					documents.set(nickname, {updates, pending, doc})
-					doc = changes.apply(doc)
-					documents.set(nickname, {updates, pending, doc})
-				}
-				socket.emit('pushUpdateResponse', true);
-
-				while (pending.length) pending.pop()!(updates)
-				documents.set(nickname, {updates, pending, doc})
-			}
-		} catch (error) {
-			console.error('pushUpdates', error)
-		}
-	})
-
-	socket.on('getDocument', ({ nickname }) => {
-		try {
-            let { updates, doc } = getDocument(nickname);
-            
-			socket.emit('getDocumentResponse', updates.length, doc.toString());
-		} catch (error) {
-			console.error('getDocument', error);
-		}
-	})
-})
-
-const port = process.env.PORT || 8000;
-server.listen(port, () => console.log(`Server listening on port: ${port}`));
+server.listen(8000, () => {
+  console.log(`WebSocket server running at on port 8000`);
+});
