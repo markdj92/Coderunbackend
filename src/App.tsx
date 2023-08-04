25000e79a5a89a1e1522335f57c84525601ccfb3
letsjo <hyunoh.jo@gmail.com>
2023-08-04 17:36:00 +0900

    [#138] Feat: init color theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Suspense } from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import PageRouter from '@/routes';
 
 import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
 
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <GlobalStyle />
-      <PageRouter />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <PageRouter />
+      </ThemeProvider>
     </Suspense>
   );
 }
