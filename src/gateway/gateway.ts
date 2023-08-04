a66f322a5282b6d2473458e962c09d55aaf0c8f1
Minji Lee <minji0479@gmail.com>
2023-08-04 17:31:35 +0900

    hotfix : leave room / quick-join

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -45,19 +45,23 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect{
 
             const token = socket.handshake.headers.authorization.split(' ')[1]; 
         
-            jwt.verify(token, process.env.JWT_SECRET, (err: any, decoded: any) => {
+            jwt.verify(token, process.env.JWT_SECRET, async (err: any, decoded: any) => {
             if (err) {
                 socket.disconnect();
                 return;
             }
             socket.decoded = decoded;
-            this.logger.log(`"token 인증 되어있습니다!`);
+                this.logger.log(`"token 인증 되어있습니다!`);
+                this.logger.log(`${socket.id} sockect connected!`);
+            const user_id = await this.userService.userInfoFromEmail(socket.decoded.email);
+            socket.user_id = user_id;
             this.authService.saveSocketId(decoded.email, socket.id);
             });
           } else {
             socket.disconnect(); // 연결을 끊음
           }
     }
+
 
     async handleDisconnect(@ConnectedSocket() socket: ExtendedSocket)  {
         const check = await this.roomService.checkWrongDisconnection(socket.decoded.email);
@@ -92,8 +96,6 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect{
         }
 
         await room.save(); 
-        const user_id = await this.userService.userInfoFromEmail(socket.decoded.email);
-        socket.user_id = user_id;
         socket.join(room.title); 
         const room_id = await this.roomService.getRoomIdFromTitle(roomCreateDto.title);
         socket.room_id = room_id;
@@ -244,40 +246,19 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect{
 
 
     @SubscribeMessage('quick-join')
-        async handleQuickJoinRoom( 
+    async handleQuickJoinRoom(
     @ConnectedSocket() socket: ExtendedSocket): 
-    Promise<{ success: boolean, payload: { roomInfo: RoomStatusChangeDto | boolean } }> {
+    Promise<{ success: boolean }> {
 
-    const email = socket.decoded.email; //token을 통해서 클라이언트의 email정보를 가져옴
-    const roomInfo = await this.roomService.findRoomForQuickJoin(email); //email정보를 매개변수로 사용자를 위한 방을 찾음    
+        const email = socket.decoded.email; 
+        const roomInfo = await this.roomService.findRoomForQuickJoin();
 
-    if (!roomInfo) {
-        return { success: false, payload: { roomInfo: false } }; //방이 없다면 실패를 반환 
-    }
-
-    this.logger.log(`${socket.id} : 방에 입장 준비 중입니다!`);
-
-    socket.join(roomInfo.title);
-    this.logger.log(`${socket.id} : Room enter!`);
-
-    const user_id = await this.userService.userInfoFromEmail(email); //email정보를 가지고 user_id를 찾고 
-    const objectId = Object(roomInfo.room_id);  // string으로 저장된 정보를 Objectid로 변경해줌->emit으로 전달해 줄 때 ObjectId값으로 찾아서 전달해줘야 하기 때문에
-
-    const isUserInRoom = await this.roomService.isUserInRoom(objectId, user_id); // quick-join의 경우 postman으로 요청을 계속 보내게 되면 요청한 유저가 중복되어 방에 접속하는 현상을 확인함 하여 다른 방에 해당 유저가 있으면 quick-join이 불가능하도록 만듬 
-        if (isUserInRoom) {
-    this.logger.log(`${socket.id} : 사용자가 이미 방에 입장했습니다!`);
-    return { success: false, payload: { roomInfo: false } };
-    }
-
-    await this.roomService.changeRoomStatusForJoin(objectId, user_id); //사용자를 방에 추가함
-    this.logger.log(`${socket.id} : 방에 입장 완료하였습니다!`);
-    const roomAndUserInfo = await this.roomService.getRoomInfo(objectId);//방과 사용자의 정보를 얻는다.
-        socket.user_id = user_id;
-        socket.room_id = objectId;
-    this.nsp.to(roomInfo.title).emit('room-status-changed', roomAndUserInfo); //클라이언트에는 join-room과 동일한 정보를 전달함
-
-    this.nsp.emit('enter-room', "enter-room!");
-    return { success: true, payload: { roomInfo: roomAndUserInfo } }; //성공과 방을 정보를 반환 
+        if (!roomInfo) {
+            return { success: false }; 
+        } 
+        const userSocketid = await this.authService.getSocketIdByuserId(socket.user_id);
+        this.nsp.to(await userSocketid).emit('readyQuickJoin', roomInfo.title);
+        return { success: true }; 
     }
 
     @SubscribeMessage('submitCode')
@@ -294,7 +275,8 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect{
             }
             const resultOutput = result.output.replace(/\n/g, '');
             userOutputResult.push(resultOutput);
-         }
+        }
+        
         if (userOutputResult.length == problem.output.length && 
             userOutputResult.every((value, index) => value == problem.output[index])) {
             await this.codingService.saveSolvedInfo(socket.decoded.email, codeSubmission.title);  
@@ -302,7 +284,7 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect{
         await this.codingService.saveSubmitInfo(socket.decoded.email, codeSubmission.title);
         const finish = await this.codingService.checkFinish(codeSubmission.title);
 
-        socket.emit('solved', { success: finish, payload: { result: result } });  
+        return { success: finish, payload: { result: result } };  
     }
 
     @SubscribeMessage('forceLeave')
