4b6a079821bb5a04dc707581e6f7eb9b8526c23c
Minji Lee <minji0479@gmail.com>
2023-08-05 15:19:05 +0000

    feat : server changed

--- codemirror-server/server.ts
+++ codemirror-server/server.ts
@@ -1,53 +1,45 @@
-const WebSocket1 = require('ws');
-const http = require('http');
-const { setupWSConnection } = require('y-websocket/bin/utils');
-const Y = require('yjs');
-
+var WebSocket1 = require('ws');
+var http = require('http');
+var setupWSConnection = require('y-websocket/bin/utils').setupWSConnection;
+var Y = require('yjs');
 // 변경한 부분: Y.Doc()을 사용해 공유 문서를 생성하고 'rooms' 키에 맵을 연결합니다.
-const ydoc = new Y.Doc();
-const rooms = ydoc.getMap('rooms');
-
+var ydoc = new Y.Doc();
+var rooms = ydoc.getMap('rooms');
 function getRoom(docName) {
-  let room = rooms.get(docName);
-  if (!room) {
-    const roomYdoc = new Y.Doc();
-    room = { ydoc: roomYdoc, clients: new Set() };
-    rooms.set(docName, room);
-  }
-  return room;
+    var room = rooms.get(docName);
+    if (!room) {
+        var roomYdoc = new Y.Doc();
+        room = { ydoc: roomYdoc, clients: new Set() };
+        rooms.set(docName, room);
+    }
+    return room;
 }
-
-const server = http.createServer((request, response) => {
-  const docName = request.url.slice(1);
-  if (rooms.has(docName)) {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify({ docName }));
-  } else {
-    response.writeHead(404);
-    response.end('Document not found');
-  }
+var server = http.createServer(function (request, response) {
+    var docName = request.url.slice(1);
+    if (rooms.has(docName)) {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify({ docName: docName }));
+    }
+    else {
+        response.writeHead(404);
+        response.end('Document not found');
+    }
 });
-
-const wss = new WebSocket1.Server({ noServer: true });
-
-wss.on('connection', (ws, req) => {
-  const docName = req.url.slice(1);
-  const room = getRoom(docName);
-  room.clients.add(ws);
-
-  ws.on('close', () => {
-    room.clients.delete(ws);
-  });
-
-  setupWSConnection(ws, req, { ydoc: room.ydoc });
+var wss = new WebSocket1.Server({ noServer: true });
+wss.on('connection', function (ws, req) {
+    var docName = req.url.slice(1);
+    var room = getRoom(docName);
+    room.clients.add(ws);
+    ws.on('close', function () {
+        room.clients.delete(ws);
+    });
+    setupWSConnection(ws, req, { ydoc: room.ydoc });
 });
-
-server.on('upgrade', (request, socket, head) => {
-  wss.handleUpgrade(request, socket, head, (ws) => {
-    wss.emit('connection', ws, request);
-  });
+server.on('upgrade', function (request, socket, head) {
+    wss.handleUpgrade(request, socket, head, function (ws) {
+        wss.emit('connection', ws, request);
+    });
 });
-
-server.listen(8000, () => {
-  console.log(`WebSocket server running on port 8000`);
+server.listen(8000, function () {
+    console.log("WebSocket server running on port 8000");
 });
