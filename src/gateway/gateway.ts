5223dcbba780d8541ca4e9a78280ca2fc64b52ce
Minji Lee <minji0479@gmail.com>
2023-08-06 06:30:52 +0000

    feat : changed reviewer

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -361,7 +361,7 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect{
                     }
                     }
                 }
-            this.nsp.to(socketId).emit('timeout', { success: true, review: true, roomInfo: roomInfo , firstReviewer : firstReviewer});
+            this.nsp.to(socketId).emit('timeout', { success: true, review: true, roomInfo: roomInfo , reviewer : firstReviewer});
             }
             else {
                 await this.roomService.resetUserStatus(socket.room_id);
