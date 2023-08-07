8ed77fa1aa29a1a4c838f4f9581bf847d285e42c
letsjo <hyunoh.jo@gmail.com>
2023-08-07 22:39:55 +0900

    [#98] Feat: changed room card detail style

--- src/components/Lobby/RoomList/RoomCard/DetailForm.tsx
+++ src/components/Lobby/RoomList/RoomCard/DetailForm.tsx
@@ -9,23 +9,35 @@ interface props {
 const DetailForm = ({ title, contents }: props) => {
   return (
     <Container>
-      <Contents>{contents}</Contents>
-      <Title>{title}</Title>
+      <Divider />
+      <DetailBox>
+        <Contents>{contents}</Contents>
+        <Title>{title}</Title>
+      </DetailBox>
     </Container>
   );
 };
 
 const Container = styled.div`
+  position: relative;
   width: 110px;
   height: 84px;
+  z-index: 0;
+  overflow: hidden;
+`;
+
+const DetailBox = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: flex-start;
+  gap: 4px;
 `;
 
 const Contents = styled.div`
-  color: #8883ff;
+  color: ${(props) => props.theme.color.MainKeyColor};
   font-family: IBM Plex Sans KR;
   font-size: 18px;
   font-style: normal;
@@ -35,13 +47,28 @@ const Contents = styled.div`
 `;
 
 const Title = styled.div`
-  color: rgba(136, 131, 255, 0.5);
+  color: ${(props) => props.theme.color.LightGray};
   font-family: 'Noto Sans KR', sans-serif;
   font-size: 14px;
   font-style: normal;
   font-weight: 500;
   line-height: 14px; /* 100% */
   letter-spacing: -0.28px;
+`;
+
+const Divider = styled.div`
+  position: absolute;
+  top: 0px;
+  left: -12px;
+  width: 90%;
+  height: 90%;
+  transform: sKewX(-15deg);
+  background:
+    linear-gradient(#26262d, #26262d) padding-box,
+    linear-gradient(to bottom, #6bd9a4, transparent) border-box,
+    border-box;
+  border-right: 2px solid transparent;
+  z-index: -1;
 `;
 
 export default DetailForm;
