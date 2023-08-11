b774e0f16d35856b14357d6322ee349e11ed0ecd
letsjo <hyunoh.jo@gmail.com>
2023-08-11 19:17:22 +0900

    [#103] Feat: add RTC Peer Connection

--- src/contexts/VoiceChatContext.tsx
+++ src/contexts/VoiceChatContext.tsx
@@ -89,16 +89,16 @@ const VoiceChatProvider = ({ children }: { children: React.ReactNode }) => {
   const makeConnection = (id, title) => {
     myPeerConnection.current[id] = new RTCPeerConnection({
       iceServers: [
-        {
-          urls: [
-            'stun:stun.l.google.com:19302',
-            'stun:stun1.l.google.com:19302',
-            'stun:stun2.l.google.com:19302',
-            'stun:stun3.l.google.com:19302',
-            'stun:stun4.l.google.com:19302',
-          ],
-        },
+        { urls: 'stun:stun.l.google.com:19302' },
+        { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'stun:stun2.l.google.com:19302' },
+        { urls: 'stun:stun3.l.google.com:19302' },
+        { urls: 'stun:stun4.l.google.com:19302' },
       ],
+      iceTransportPolicy: 'all',
+      bundlePolicy: 'balanced',
+      rtcpMuxPolicy: 'require',
+      iceCandidatePoolSize: 0,
     });
 
     myPeerConnection.current[id].addEventListener('icecandidate', (data) =>
